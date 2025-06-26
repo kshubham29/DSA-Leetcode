da499c9c01b2e4e3606e781357e1509f538f0901
Shubham Kumar <157146556+kshubham29@users.noreply.github.com>
2025-06-26 22:08:01 +0530

    Time: 200 ms (73.09%), Space: 41.3 MB (34.52%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,40 @@
+public class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) return true;
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num)
+                return false;
+        }
+        int boxRowStart = 3 * (row / 3);
+        int boxColStart = 3 * (col / 3);
+        for (int r = boxRowStart; r < boxRowStart + 3; r++) {
+            for (int c = boxColStart; c < boxColStart + 3; c++) {
+                if (board[r][c] == num)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
