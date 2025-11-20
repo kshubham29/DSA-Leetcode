bf216ae2e23571d891e604172c8f733fc3867490
Shubham Kumar <157146556+kshubham29@users.noreply.github.com>
2025-11-20 23:21:02 +0530

    Time: 27 ms (66.83%), Space: 47.4 MB (17.54%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<Pair> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek().ch == c) {
+                stack.peek().count++;
+                if (stack.peek().count == k) stack.pop();
+            } else {
+                stack.push(new Pair(c, 1));
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (Pair p : stack) {
+            for (int i = 0; i < p.count; i++) sb.append(p.ch);
+        }
+        return sb.toString();
+    }
+}
+
+class Pair {
+    char ch;
+    int count;
+    Pair(char c, int cnt) {
+        ch = c;
+        count = cnt;
+    }
+}
