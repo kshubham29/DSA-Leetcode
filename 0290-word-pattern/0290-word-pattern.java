186c9e2973db3722f469c7a9cd43c37bbfa9bdf6
Shubham Kumar <157146556+kshubham29@users.noreply.github.com>
2025-11-30 23:37:45 +0530

    Time: 1 ms (80.14%), Space: 42.7 MB (21.06%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) return false;
+
+        Map<Character, String> map1 = new HashMap<>();
+        Map<String, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String w = words[i];
+
+            if (map1.containsKey(c) && !map1.get(c).equals(w)) return false;
+            if (map2.containsKey(w) && map2.get(w) != c) return false;
+
+            map1.put(c, w);
+            map2.put(w, c);
+        }
+        return true;
+    }
+}
