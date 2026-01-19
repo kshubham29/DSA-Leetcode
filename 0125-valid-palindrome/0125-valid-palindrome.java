6042b2060fddfb531067f76ee673ba7c46810846
Shubham Kumar <157146556+kshubham29@users.noreply.github.com>
2026-01-19 21:38:22 +0530

    Time: 2 ms (98.81%), Space: 44.4 MB (63.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,18 @@
-public class Solution {
+class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder cleaned = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                cleaned.append(Character.toLowerCase(c));
-            }
-        }
-        String filtered = cleaned.toString();
-        String reversed = cleaned.reverse().toString();
-        return filtered.equals(reversed);
-    }
+        int l = 0, r = s.length() - 1;
 
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.isPalindrome("A man, a plan, a canal: Panama")); 
-        System.out.println(sol.isPalindrome("race a car"));                     
-        System.out.println(sol.isPalindrome(" "));                              
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) l++;
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) r--;
+
+            if (Character.toLowerCase(s.charAt(l)) !=
+                Character.toLowerCase(s.charAt(r)))
+                return false;
+
+            l++;
+            r--;
+        }
+        return true;
     }
 }
