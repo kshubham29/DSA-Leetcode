b2f29153843fa3409ff34812406f930cb48120af
Shubham Kumar <157146556+kshubham29@users.noreply.github.com>
2026-02-17 08:50:26 +0530

    Time: 0 ms (100%), Space: 45.5 MB (70.08%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -1,55 +1,30 @@
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-
-    public static ListNode deserialize(String data) {
-        if (data == null || data.length() <= 2) return null;
-        String[] vals = data.substring(1, data.length() - 1).split(",");
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        for (String val : vals) {
-            current.next = new ListNode(Integer.parseInt(val.trim()));
-            current = current.next;
-        }
-        return dummy.next;
-    }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        ListNode curr = this;
-        while (curr != null) {
-            sb.append(curr.val);
-            if (curr.next != null) sb.append(",");
-            curr = curr.next;
-        }
-        sb.append("]");
-        return sb.toString();
-    }
-}
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode current = head;
-        while (current != null && current.next != null) {
-            if (current.val == current.next.val) {
-                current.next = current.next.next;
-            } else {
-                current = current.next;
-            }
-        }
-        return head;
-    }
-}
+            ListNode temp = head;
+             while (temp != null && temp.next != null){
+            if (temp.val == temp.next.val){
+                ListNode dup = temp.next;
+                temp.next = temp.next.next;
+                dup.next = null;
 
-public class Main {
-    public static void main(String[] args) {
-        ListNode head = ListNode.deserialize("[1,1,2]");
-        Solution solution = new Solution();
-        ListNode result = solution.deleteDuplicates(head);
-        System.out.println(result);
+            }else 
+                {
+                    temp= temp.next;
+                }
+
+
+        
     }
+    return head;
+}
 }
